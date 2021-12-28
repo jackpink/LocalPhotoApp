1400a68ca144e338436fe0ebc0c2060cbca66076
Jack-Desktop <jackpinkerton@live.com.au>
2021-12-28 13:39:50 +1100

    header updated

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+
 
 const Header = () => {
     return(
-        <div className="header">header</div>
+        <div className="header">
+            <AppBar position="static">
+                <Typography variant="h2" component="div" sx={{ flexGrow: 1 }}>
+                    Local Photo App
+                </Typography>
+            </AppBar>
+        </div>
     )
 }
 
