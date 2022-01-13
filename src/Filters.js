98705d534d128486fd329bcbe6ae0ec984be6c02
Jack-Desktop <jackpinkerton@live.com.au>
2022-01-13 13:47:46 +1100

    Get albums from backend implemented on page load

--- src/Filters.js
+++ src/Filters.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AlbumSelector from "./AlbumSelector";
 import CreateAlbum from "./CreateAlbum";
+import { getAlbums } from "./backend";
 
 
 const Filters = () => {
@@ -8,6 +9,21 @@ const Filters = () => {
     const [currentAlbums, setCurrentAlbums] = useState([]);
 
     // Will have a useEffect hook here to get albums 
+    const getAlbumsFromBackend = async () => {
+        const albumsFromBackend = await getAlbums();
+        console.log('res', albumsFromBackend);
+        setAlbums(albumsFromBackend);
+    }
+
+
+    useEffect(() => {
+        try {
+            getAlbumsFromBackend();
+            
+        } catch {
+            console.log("didn't get from backend");
+        }
+    }, [])
 
     return(
         <div className="filters">
---
+++ src/backend.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const url = 'http://127.0.0.1:8081/';
+const albumsURL = url + 'albums';
+const photosURL = url + 'photos';
+
+// backend.getAlbums()
+
+export const getAlbums = async () => {
+    try {
+        const response = await axios.get(albumsURL);
+        console.log('backend before return', response.data.albums);
+        return response.data.albums;
+    } catch (err) {
+        console.log('get albums error', err)
+        return ['ablum'];
+    }
+}
+
+// backend.create_album(albumName)
+
+export const createAlbum = async (albumName) => {
+   const newAlbum = { "name" : albumName };
+   axios({
+       method: 'post',
+       url: albumsURL,
+       data: {
+           newAlbum
+       }
+   })
+   .then(data=>console.log(data))
+   .catch(err=>console.log(err))
+}
+
+// backend.getPhotos()
+
+// backend.uploadPhoto(file, album)
